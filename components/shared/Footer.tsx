fbc419f389a2fae49cfc4e5e0359621bc0dd4d5f
sultanmalik1 <sullionik@gmail.com>
2024-05-02 17:43:12 -0400

    connected to Mongodb. Created two files user.model.ts and event.model.ts

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -6,12 +6,7 @@ const Footer = () => {
     <footer className="border-t">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href="/">
-          <Image
-            src="/assets/images/logo.svg"
-            alt="logo"
-            width={128}
-            height={38}
-          />
+          <Image src="/images/logo.svg" alt="logo" width={128} height={38} />
         </Link>
 
         <p>2024 EventfulGiving. All Rights reserved.</p>
